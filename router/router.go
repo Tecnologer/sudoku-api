ec58a569113e3380790d187acaf14ab04cf80dbf
Rey David <rdominguez@tecnologer.net>
2021-01-26 23:30:15 -0600

    router: add setValue and levels

--- router/router.go
+++ router/router.go
@@ -10,7 +10,8 @@ func Router() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/game", middleware.NewGame).Methods("GET", "OPTIONS")
-	// router.HandleFunc("/api/game/set", middleware.SetValue).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/game/set", middleware.SetValue).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/game/levels", middleware.GetLevels).Methods("GET", "OPTIONS")
 	// router.HandleFunc("/api/game/validate", middleware.Validate).Methods("GET", "OPTIONS")
 	// router.HandleFunc("/api/game/solve", middleware.solve).Methods("GET", "OPTIONS")
 
